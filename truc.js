b602e5a97f01824f2c139543c8370edda90c4bad
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-11-26 15:28:59 +0100

    1st rev

---
+++ truc.js
@@ -0,0 +1,19 @@
+function Question(truc)
+{
+    let prenom = prompt("Quel est ton nom ?")
+    let phrase = "Bonjour " + prenom + "!" + truc
+
+    console.log(prenom)
+
+    alert(phrase)
+}
+
+function Hello()
+{
+    alert(ici.value)
+}
+
+function Remplacer()
+{
+    carabistouille.innerHTML = ici.value
+}
